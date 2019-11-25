602b3691eabcc085eb9fa649a11644adb8a8411a
Fartonaut <brandonsboom@gmail.com>
2019-11-24 16:04:00 -0800

    a bit of progress

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
-#include <stdio.h>	
+#include <stdio.h>	//to print status stuff
+#include <stdint.h>	//don't be silly, wrap your willy
 #include <string.h>	//for string stuff
 #include <sys/types.h>	//needed on some legacy systems
 #include <sys/socket.h>	//needs above on legacy sytems
+#include <netinet/in.h>	//
+#include <netinet/ip.h> //soup set of ^
+#include <errno.h>	//no error
 
 //super secret stuffz
-const char *username = "Fartonaut";
-const char *password = "hunter2";
-char realname[11];
+const char *username = "FartoBot";
+
+//consts
+const uint16_t port = 6667;
+const char *server = "irc.esper.net";
+
+//not consts
+bool connected = false;
+char password[16];
+
 //establish connection to interwebz
-socket(AF_INET, SOCK
+int soccSucc = socket(AF_INET, SOCK_STREAM, 0);
 
 int main() {
-	
+	printf("Enter irc password: ");
+	scanf("%16s", &password);
 	return 0;
 }
